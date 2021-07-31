d718b18a020ce54f3db819ec6aa3b3a317cfee4e
Will2021Expleo <81624212+Will2021Expleo@users.noreply.github.com>
2021-07-31 08:05:56 -0400

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from '../js/input.js'
 
-export const SNAKE_SPEED = 4 //pour faire varier la vitesse du bleu
+export const SNAKE_SPEED = 2 //pour faire varier la vitesse du bleu
 const snakeBody = [{ x: 11, y: 11 }]
 let newSegments = 0
 
